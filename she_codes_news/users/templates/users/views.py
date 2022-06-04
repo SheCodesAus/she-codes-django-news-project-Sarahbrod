da418424bbf083d327e8125e99c0d7242a668b5b
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-06-04 12:36:22 +1000

    !!!

--- she_codes_news/users/templates/users/views.py
+++ she_codes_news/users/templates/users/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views import generic
-
 from .models import CustomUser
 from .forms import CustomUserCreationForm
 
