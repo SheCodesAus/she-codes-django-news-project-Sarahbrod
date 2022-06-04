08748fd83084cdba147d1a611c2a8d42ee8b9dd2
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-06-04 10:55:40 +1000

    Part 2 :)
    the the part is linking the submit story to a working page

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'news:index'
+LOGOUT_REDIRECT_URL = 'news:index'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,
